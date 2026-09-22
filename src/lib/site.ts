e2a25984a322aacd7902e470bcadbbbdf079f1ae
Sebastian Ilbay <atlantiscityplay@gmail.com>
2026-09-22 14:03:39 -0500

    Update site.ts

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: "TechToJob",
-  discordUrl: "https://discord.gg/HTKMZHn4W?event=1538953727136759959",
+  discordUrl: "https://discord.com/invite/h9FFgKdkRd",
   githubUrl: "https://github.com/ATLASSdeveloper",
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL ?? "https://techtojob.vercel.app",
 };
